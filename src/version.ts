df9fb07fc003f5d4018e38afce631a4e3cf307f4
Kabir Sekhon <tjaynj@gmail.com>
2026-06-22 06:07:55 -0400

    remove docs

--- src/version.ts
+++
@@ -1,9 +0,0 @@
-import packageJson from "../package.json";
-
-export const STICKS_LITE_VERSION = packageJson.version;
-
-export function withVersionPlaceholders(markdown: string): string {
-  return markdown
-    .replace(/\\`/g, "`")
-    .replace(/\{\{VERSION\}\}/g, STICKS_LITE_VERSION);
-}
--- src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module "*.md?raw" {
-  const source: string;
-  export default source;
-}
